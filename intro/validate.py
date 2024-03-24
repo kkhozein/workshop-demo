8c2c688f1d535cabe47f2e5ca4643f6dd79c5672
K Khozein <kkhozein@madeira.org>
2024-03-23 17:16:17 -0700

    added unit testing

--- intro/validate.py
+++ intro/validate.py
@@ -1,6 +1,12 @@
 # Always start by importing Pedal
 from pedal import *
 
-
+# example from the workshop
+unit_test('cube_elements',
+            ([[1, 2, 3]], [1, 8, 27]),
+            ([[1]], [1]),
+            ([[4, 4, 4]], [64, 64, 64]),
+            ([[0]], [0]),
+            ([[]], []))
 
 # ... More instructor logic can go here ...
